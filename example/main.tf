e75a854938ae9788aa556818e5ed607f828d5f1a
Yakhya Dabo <yakhyadabo@gmail.com>
2023-02-25 01:05:48 +0000

    [example] Module usage

---
+++ example/main.tf
@@ -0,0 +1,14 @@
+resource "random_id" "unique_suffix" {
+  byte_length = 2
+}
+
+locals {
+  app_id = "${lower(var.project.name)}-${lower(var.project.environment)}-${random_id.unique_suffix.hex}"
+}
+
+module "lambda" {
+  source               = "../module"
+  app_id               = local.app_id
+  project_name         = var.project.name
+  environment          = var.project.environment
+}
---
+++ example/outputs.tf
@@ -0,0 +1,3 @@
+output "api_url" {
+  value = module.lambda.api_url
+}
